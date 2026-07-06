194c5dc0aa45b3866237f74e68759239930b6c51
Ivan <durin.s_son@mail.ru>
2026-07-06 14:24:54 +0300

    Add TestController and update analyzer settings
    Added TestController with a GET endpoint at api/test returning HTTP 200 OK. Disabled CA1515 analyzer in .globalconfig. Added BOM to FirstService.Web.csproj.

---
+++ backend/FirstService/src/FirstService.Web/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstService.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public sealed class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInfo()
+        {
+            return Ok();
+        }
+    }
+}
